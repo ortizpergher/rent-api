88e43e2856775d8ddc4059e917df39d849aada96
Ortiz Pergher <ortizpergher@gmail.com>
2022-04-05 22:41:00 -0300

    created tests, useCase, controller rents

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,76 @@
+import dayjs from 'dayjs';
+
+import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+import { CreateRentalUseCase } from './CreateRentalUseCase';
+
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let createRentalUseCase: CreateRentalUseCase;
+let dayjsDateProvider: DayjsDateProvider;
+
+describe('Create Rental', () => {
+  const dayAdd24Hours = dayjs().add(24, 'hours').toDate();
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    dayjsDateProvider = new DayjsDateProvider();
+    createRentalUseCase = new CreateRentalUseCase(
+      rentalsRepositoryInMemory,
+      dayjsDateProvider
+    );
+  });
+
+  it('should be able to create a rental', async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: 'user_id-123',
+      car_id: 'car_id-123',
+      expect_return_date: dayAdd24Hours
+    });
+
+    expect(rental).toHaveProperty('id');
+    expect(rental).toHaveProperty('start_date');
+  });
+
+  it('should not be able to create a rental if user has rent open', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: 'user_id-148',
+        car_id: 'car_id-158',
+        expect_return_date: dayAdd24Hours
+      });
+
+      await createRentalUseCase.execute({
+        user_id: 'user_id-148',
+        car_id: 'car_id-159',
+        expect_return_date: dayAdd24Hours
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a rental if car has a rent open', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: 'user_id-148',
+        car_id: 'car_id-15845',
+        expect_return_date: dayAdd24Hours
+      });
+
+      await createRentalUseCase.execute({
+        user_id: 'user_id-149',
+        car_id: 'car_id-15845',
+        expect_return_date: dayAdd24Hours
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a rental with invalid return time', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: 'user_id-741',
+        car_id: 'car_id-742',
+        expect_return_date: dayjs().toDate()
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/shared/infra/database/migrations/1646616004780-CreateUsers.ts
+++ src/shared/infra/database/migrations/1646616004780-CreateUsers.ts
@@ -8,7 +8,8 @@ export class CreateUsers1646616004780 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'uuid'
+            type: 'uuid',
+            isPrimary: true
           },
           {
             name: 'name',
